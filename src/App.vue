77b7a30980d0689cc2ad37e317eed3d999c08431
Fabien <fabiolo.aori@gmail.com>
2023-12-13 17:01:57 +0100

    second push

--- src/App.vue
+++ src/App.vue
@@ -5,33 +5,35 @@ export default {
   data() {
     return {
       score: 0,
+      score_one: 0,
+      score_two:0,
+      score_three: 0,
       random: 0,
       items:[0,0,0,0,0,0,0,0,0],
-      caseJeu: 0
+      
     }
   },
   methods: {
     lancer() {
       this.random = Math.ceil(Math.random() * 6)
-      console.log(this.random)
-      // this.game = this.random
-      // console.log(this.game)
+      console.log(this.items)
     },
+    afficherValeur(item,index){      
+      this.items[index] = this.random;
+      this.score = this.score_one
+      this.score_one = this.items[0] + this.items[3] +this.item[6]
+      // this.score+=this.items[index]
+      console.log(this.items[0])
 
-    afficherValeur(item,index){
-      
-      this.items[index] = this.random; 
-    
     }
-
-
   }
 }
 </script>
 
 <template>
-  <div v-for="(item, index) in items" :key="index" @click="afficherValeur(item,index)">{{ item }}</div>
-
+  <div class="plateau">
+  <div class="case" v-for="(item, index) in items" :key="index" @click="afficherValeur(item,index)">{{ item }}</div>
+  </div>
   <div class="score">{{ score }}</div>
 
   <div class="de">
@@ -49,7 +51,23 @@ export default {
 }
 
 button {
+  position: absolute;
+  right: 50%;
   width: 45px;
   height: 45px;
+}
+
+.plateau{
+display: grid;
+grid-template-columns: 100px 100px 100px;
+  background-color: red;
+  height: 300px;
+  width: 300px;
+}.case{
+
+  width: 100px;
+  height: 100px;
+  border: 1px solid;
+  border-color: black;
 }
 </style>
